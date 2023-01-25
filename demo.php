b42de33e23ec1e6b2cff7ca8a67ab14cbcebc616
sachiyo <sachiyo.sable@gmail.com>
2023-01-25 11:45:05 +0100

    Cp6 les tableaux de notes

--- demo.php
+++ demo.php
@@ -1,21 +1,15 @@
-Salut les gens
 <?php
-$prenom = 'Sachiyo';
-$nom = 'Sablé';
-echo $prenom . "\n" . $nom . "\n"; // si '\n', affiche comme ça. il faut utiliser ""
-echo "$prenom\n$nom\n";
-?>
-Comment ça va?
+$eleve = [
+    'nom' => 'Doe',
+    'prenom' => 'Marc',
+    'notes' => [10, 20, 15]
+];
+echo $eleve['prenom'] = 'Jean';
+echo $eleve['notes'][] = 16;
+echo $eleve['notes'][] = 16;
+echo $eleve['notes'][] = 16;
+echo $eleve['notes'][] = 16;
 
-<?php
-// exo 1
-$prenom = 'Marc';
-$nom = 'Doe';
-$note1 = 10;
-$note2 = 20;
-$moyenne = ($note1 + $note2) / 2 ;
-
-echo 'Bonjour ' . $prenom . ' ' . $nom . ' vous avez eu ' . ($note1 + $note2) / 2 . ' de moyenne';
-echo "\nBonjour $prenom $nom vous avez eu $moyenne de moyenne";
+print_r ($eleve['notes']);
 
 ?>
