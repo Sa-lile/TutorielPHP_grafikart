611499d3d1db0df091d17a419a77e09111824fbd
sachiyo <sachiyo.sable@gmail.com>
2023-01-25 13:33:28 +0100

    Cp8 les boucles d'eleves

--- demo.php
+++ demo.php
@@ -1,10 +1,14 @@
 <?php
 $notes = [10, 15, 16];
 $eleves = [
-    'cm2' => 'Jean',
-    'cm1' => 'Marc',
+    'cm2' => ['Jean', 'Marc', 'Jane', 'Marion'], // 'cm2' est key = $classe
+    'cm1' => ['Emilie','Marcelin'],
 ];
-foreach ($eleves as $classe => $eleve) {
-    echo " $eleve est dans la classe $classe \n";
+
+foreach ($eleves as $classe => $listEleves) {
+    echo " La classe $classe:\n";
+    foreach ($listEleves as $eleve) {
+    echo "- $eleve\n";
+    }
+    echo "\n";
 }
-?>
