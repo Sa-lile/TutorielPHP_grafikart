f5fd907246ef8549e23ab9888942f7a273ffdf5d
sachiyo <sachiyo.sable@gmail.com>
2023-01-26 10:08:51 +0100

    créer les fichiers de creneaux

---
+++ contact.php
@@ -0,0 +1,8 @@
+<?php require 'header.php'; ?>
+
+<h2>Nous contacter</h2>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde maxime optio rerum nostrum eius labore a ab eaque facilis ut.</p>
+
+
+<?php require 'footer.php'; ?>
--- demo.php
+++ demo.php
@@ -1,47 +1,11 @@
-<?php
+<?php require 'header.php'; ?>
 
-// declare(strict_types=1);
+<main class="container">
+  <div class="bg-light p-5 rounded">
+    <h1>Navbar example</h1>
+    <p class="lead">This example is a quick exercise to illustrate how fixed to top navbar works. As you scroll, it will remain fixed to the top of your browser’s viewport.</p>
+    <a class="btn btn-lg btn-primary" href="/docs/5.3/components/navbar/" role="button">View navbar docs &raquo;</a>
+  </div>
+</main>
 
-function repondre_oui_non (string $phrase): bool {
-  while(true){
-    $reponse = readline($phrase . " - (o)ui/(n)on : ");
-    if ($reponse === "o") {
-        return true;
-    } elseif ($reponse === 'n') {
-        return false;
-        }
-    }
-}
-
-function demander_creneau ($phrase = 'Veuillez entrer votre creneau') {
-    echo $phrase . "\n";
-
-    while (true) {
-        $ouverture = (int)readline('Heure d\'ouverture : ');
-        if ($ouverture >= 0 && $ouverture <= 23) {
-            break;
-        }
-    }
-    while (true) {
-        $fermeture = (int)readline('Heure de fermeture : ');
-        if ($fermeture >= 0 && $fermeture <= 23 && $fermeture > $ouverture) {
-            break;
-        }
-    }
-    return [$ouverture, $fermeture];
-}
-
-function demander_creneaux() {
-    $creneaux = [];
-    $continuer =  true;
-    while ($continuer) {
-        $creneaux[] = demander_creneau();
-        $continuer = repondre_oui_non('Voulez vous continuer ?');
-    }
-    var_dump($creneaux);
-    for( $i = 0; $i < count($creneaux); $i++ ){
-        echo "Jour ".$i." : " . $creneaux[$i][0] ." -> ". $creneaux[$i][1] . "\n";
-    }
-}
-demander_creneaux();
-
+<?php require 'footer.php'; ?>
