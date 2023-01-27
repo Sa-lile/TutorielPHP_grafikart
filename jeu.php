f1d70de197ba1584620a50fc6a6292a9b617d008
sachiyo <sachiyo.sable@gmail.com>
2023-01-27 16:02:42 +0100

    Crée le fichier jeu

--- jeu.php
+++ jeu.php
@@ -1,9 +1,18 @@
 <?php
-$aDeviner = 160;
+$aDeviner = 150;
 require 'header.php';
 ?>
+
+<?php if($_GET['chiffre'] > $aDeviner): ?>
+    Votre chiffre est trop grand
+<?php elseif($_GET['chiffre'] < $aDeviner): ?>
+    Votre chiffre est trop petit
+<?php else: ?>
+    Bravo ! Vous avez deviné le chiffre <?= $aDeviner;?>
+<?php endif ?>
+
 <form action="/jeu.php" method="GET">
-    <input type="number" name="chiffre" placeholder="entre 0 et 1000">
+    <input type="number" name="chiffre" placeholder="entre 0 et 1000" value="<?=$_GET['chiffre']?>">
     <button type="submit">Deviner</button>
 </form>
 
