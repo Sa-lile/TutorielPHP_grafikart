c8c9f8eadc09b07773d441f0f7732eb677acc552
sachiyo <sachiyo.sable@gmail.com>
2023-01-27 11:25:44 +0100

    Creé le fichier menu et ajouté heredoc

--- contact.php
+++ contact.php
@@ -6,7 +6,7 @@ require 'header.php';
 
 <h2>Nous contacter</h2>
 
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde maxime optio rerum nostrum eius labore a ab eaque facilis ut.</p>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia possimus repudiandae alias, sunt consectetur impedit rem itaque debitis dolorem nobis eaque eos deserunt cupiditate iusto asperiores sapiente minima obcaecati dolorum.</p>
 
 
 <?php require 'footer.php'; ?>
